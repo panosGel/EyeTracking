dc0a897771e8297ebdce2a6dfeb7da5125bab752
panosGel <panosgel@gmail.com>
2014-08-06 19:57:54 +0100

    plot fix

--- datasetDivision.py
+++ datasetDivision.py
@@ -7,90 +7,71 @@ import csv
 import re
 from os import path
 
-
-file = open(STATS_FOLDER+"demData.tsv")
-#dictionary for the paintings
-paintingsDict = dict([(1, [CHEVIOT_FARM_1,CHEVIOT_FARM_2,CHEVIOT_FARM_3]), (2, [NOW_FOR_THE_PAINTER_1,NOW_FOR_THE_PAINTER_2,NOW_FOR_THE_PAINTER_3]),
+def divide_datasets():
+    file = open(STATS_FOLDER+"demData.tsv")
+    #dictionary for the paintings - each variable contains the recordings of the
+    #corresponding familiarity level.
+    paintingsDict = dict([(1, [CHEVIOT_FARM_1,CHEVIOT_FARM_2,CHEVIOT_FARM_3]), (2, [NOW_FOR_THE_PAINTER_1,NOW_FOR_THE_PAINTER_2,NOW_FOR_THE_PAINTER_3]),
                       (3, [KONIGSTEIN_1,KONIGSTEIN_2,KONIGSTEIN_3]),(4,[WOMAN_AND_SUSPENDED_MAN_1,WOMAN_AND_SUSPENDED_MAN_2,WOMAN_AND_SUSPENDED_MAN_3]),
                       (5,[FOURTEEN_SIX_1964_1,FOURTEEN_SIX_1964_2,FOURTEEN_SIX_1964_3]),(6,[CHEETAH_STAGS_1,CHEETAH_STAGS_2,CHEETAH_STAGS_3]),
                       (7,[FLASK_WALK_1,FLASK_WALK_2,FLASK_WALK_3]),(8,[RELEASE_1,RELEASE_2,RELEASE_3]),(9,[SELF_PORTRAIT_1,SELF_PORTRAIT_2,SELF_PORTRAIT_3]),
                       (10,[WEST_WITH_EVENING_1,WEST_WITH_EVENING_2,WEST_WITH_EVENING_3]),(12,[SIR_GREGORY_1,SIR_GREGORY_2,SIR_GREGORY_3]),
                       (11,[RHYL_SANDS_FAMILIARITY_1,RHYL_SANDS_FAMILIARITY_2,RHYL_SANDS_FAMILIARITY_3])])
 
-try:
-    reader = csv.reader(file,delimiter='\t')
-    for row in reader:
+    try:
+        reader = csv.reader(file,delimiter='\t')
+        for row in reader:
 
-        #get the recording file
-        participantNum = row[0]
-        recordingNum = participantNum[:-1]
-        recordingFile = 'Rec ' + recordingNum + '-All-Data.tsv'
+            #get the recording file
+            participantNum = row[0]
+            recordingNum = participantNum[:-1]
+            recordingFile = 'Rec ' + recordingNum + '-All-Data.tsv'
 
-        #add recording to proper gender dataset
-        if row[13] == "Female":
-            FEMALE_PARTICIPANTS.append(recordingFile)
-        else  :
-            MALE_PARTICIPANTS.append(recordingFile)
+            #add recording to proper gender dataset
+            if row[13] == "Female":
+                FEMALE_PARTICIPANTS.append(recordingFile)
+            else  :
+                MALE_PARTICIPANTS.append(recordingFile)
 
         #add recording to proper age group dataset
 
-        if row[14] == "1":
-            AGEGROUP_1_PARTICIPANTS.append(recordingFile)
-        elif row[14] == "2":
-            AGEGROUP_2_PARTICIPANTS.append(recordingFile)
+            if row[14] == "1":
+                AGEGROUP_1_PARTICIPANTS.append(recordingFile)
+            elif row[14] == "2":
+                AGEGROUP_2_PARTICIPANTS.append(recordingFile)
 
-        elif row[14] == "3":
-            AGEGROUP_3_PARTICIPANTS.append(recordingFile)
+            elif row[14] == "3":
+                AGEGROUP_3_PARTICIPANTS.append(recordingFile)
 
-        elif row[14] == "4":
-            AGEGROUP_4_PARTICIPANTS.append(recordingFile)
+            elif row[14] == "4":
+                AGEGROUP_4_PARTICIPANTS.append(recordingFile)
 
-        else:
-            AGEGROUP_5_PARTICIPANTS.append(recordingFile)
-        #add recording to proper level of interest group dataset
-        if row[16] == "very interested":
-            INTEREST_GROUP_1_VERY_INTERESTED.append(recordingFile)
-        elif row[16] == "quite interested":
-            INTEREST_GROUP_2_QUITE_INTERESTED.append(recordingFile)
-        elif row[16] == "moderate interest":
-            INTEREST_GROUP_3_MODERATE_INTEREST.append(recordingFile)
-        elif row[16] == "not very interested":
-            INTEREST_GROUP_4_NOT_VERY_INTERESTED.append(recordingFile)
-
-        #familiarity of each artwork
-        #
-        for i in range(1,12):
-            if row[i] == "1":
-                paintingsDict.get(i)[0].append(recordingFile)
-            elif row[i] == "2":
-                paintingsDict.get(i)[1].append(recordingFile)
             else:
-                paintingsDict.get(i)[2].append(recordingFile)
+                AGEGROUP_5_PARTICIPANTS.append(recordingFile)
+            #add recording to proper level of interest group dataset
+            if row[16] == "very interested":
+                INTEREST_GROUP_1_VERY_INTERESTED.append(recordingFile)
+            elif row[16] == "quite interested":
+                INTEREST_GROUP_2_QUITE_INTERESTED.append(recordingFile)
+            elif row[16] == "moderate interest":
+                INTEREST_GROUP_3_MODERATE_INTEREST.append(recordingFile)
+            elif row[16] == "not very interested":
+                INTEREST_GROUP_4_NOT_VERY_INTERESTED.append(recordingFile)
+
+            #familiarity of each artwork
+            #
+            for i in range(1,13):
+                if row[i] == "1":
+                    paintingsDict.get(i)[0].append(recordingFile)
+                elif row[i] == "2":
+                    paintingsDict.get(i)[1].append(recordingFile)
+                else:
+                    paintingsDict.get(i)[2].append(recordingFile)
 
 
 
-finally:
-    file.close()
+    finally:
+        file.close()
 
-print "gender"
-print len(FEMALE_PARTICIPANTS)
-print len(MALE_PARTICIPANTS)
-print "age group"
-print len(AGEGROUP_1_PARTICIPANTS)
-print len(AGEGROUP_2_PARTICIPANTS)
-print len(AGEGROUP_3_PARTICIPANTS)
-print len(AGEGROUP_4_PARTICIPANTS)
-print len(AGEGROUP_5_PARTICIPANTS)
-print "interest groups"
-print len(INTEREST_GROUP_1_VERY_INTERESTED)
-print len(INTEREST_GROUP_2_QUITE_INTERESTED)
-print len(INTEREST_GROUP_3_MODERATE_INTEREST)
-print len(INTEREST_GROUP_4_NOT_VERY_INTERESTED)
-print "cheviot farm familiarity"
-print len(CHEVIOT_FARM_1)
-print len(CHEVIOT_FARM_2)
-print len(CHEVIOT_FARM_3)
-print "now for the painter familiarity"
-print len(NOW_FOR_THE_PAINTER_1)
-print len(NOW_FOR_THE_PAINTER_2)
-print len(NOW_FOR_THE_PAINTER_3)
+    return paintingsDict
+
--- viewerDataPlots.py
+++ viewerDataPlots.py
@@ -1,1 +1,116 @@
 __author__ = 'panos'
+import numpy as np
+import matplotlib.pyplot as plt
+import datasetDivision as div
+from ini import *
+
+
+paintingsDictionary = div.divide_datasets()
+
+def gender_plot():
+
+    N = 1
+    menNum = (len(MALE_PARTICIPANTS))
+    womenNum = (len(FEMALE_PARTICIPANTS))
+
+
+    width = 0.35
+
+    fig,ax = plt.subplots()
+
+    men_rectangle = ax.bar(0, menNum, width, color='r',align='center')
+    women_rectangle = ax.bar(width,womenNum,width,color='y',align='center')
+
+    ax.set_ylabel('Viewers')
+    ax.set_title('Viewers by gender')
+    plt.show()
+
+def agegroups_plot():
+    print "age groups"
+
+def interest_plot():
+    interest1 = [len(INTEREST_GROUP_1_VERY_INTERESTED)]
+    interest2 = [len(INTEREST_GROUP_2_QUITE_INTERESTED)]
+    interest3 = [len(INTEREST_GROUP_3_MODERATE_INTEREST)]
+    interest4 = [len(INTEREST_GROUP_4_NOT_VERY_INTERESTED)]
+
+    N=1
+
+    index = np.arange(N)
+    margin = 0.05
+    width = (2.-4.*margin)/N
+    #width = (0.1-0.2*margin)/N
+    opacity = 0.3
+    fig, ax = plt.subplots()
+    xdata = index+margin+(N*width)
+
+    int1 = plt.bar(index+margin+(1*width), interest1, width,
+                 alpha=opacity,
+                 color='b',
+                 label="Very interested")
+    int2 = plt.bar(index+margin+(2*width), interest2, width,
+                 alpha=opacity,
+                 color='r',
+                 label="Quite interested")
+    int3 = plt.bar(index+margin+(3*width), interest3, width,
+                 alpha=opacity,
+                 color='y',
+                 label="Moderate interest")
+    int4 = plt.bar(index+margin+(4*width), interest4, width,
+                 alpha=opacity,
+                 color='g',
+                 label="Not very interested")
+    plt.xlabel('Interest in art')
+    plt.ylabel('Viewers')
+    plt.title("Interest of Viewers in art")
+    plt.xticks(index+0.5, ('Very\nInterested',"Quite\ninterested","Moderate\nInterest","Not very\nInterested"))
+
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def paintingsFamiliarityPlots():
+    N = 12
+
+
+    familiarity1 = [len(paintingsDictionary.get(i)[0]) for i in range (1,13)]
+    familiarity2 = [len(paintingsDictionary.get(i)[1]) for i in range (1,13)]
+    familiarity3 = [len(paintingsDictionary.get(i)[2]) for i in range (1,13)]
+    print familiarity1
+
+    index = np.arange(N)
+    margin = 0.05
+    width = (2.-4.*margin)/N
+    #width = (0.1-0.2*margin)/N
+    opacity = 0.3
+    fig, ax = plt.subplots()
+    xdata = index+margin+(N*width)
+    fam1 = plt.bar(index+margin+(1*width), familiarity1, width,
+                 alpha=opacity,
+                 color='b',
+                 label="I don\'t recall ever seeing this artwork before")
+    fam2 = plt.bar(index+margin+(2*width),familiarity2,width,
+                 alpha=opacity,
+                 color='r',
+                 label="I have seen this artwork once or twice before")
+    fam3 = plt.bar(index+margin+(3*width),familiarity3,width,
+                 alpha=opacity,
+                 color='y',
+                 label="I have seen this artwork many times before")
+
+
+    plt.xlabel('Paintings')
+    plt.ylabel('Viewers')
+    plt.title("Familiarity with each painting")
+    plt.xticks(index+0.5, ('Cheviot \nFarm',"Now for\n the painter","Konigstein","Women and\n suspended man","14.06.1964",
+                               "Cheetah and\n two indians",
+                               "Flask walk","Release","Self portrait","Evening glows","Rhyl Sands","Sir Gregory"))
+
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+
+
